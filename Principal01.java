ce34e23e15f198b7bac057ffd03b1695f85b1189
AlbertoDaw23 <159157814+AlbertoDaw23@users.noreply.github.com>
2024-02-06 16:25:36 +0100

    Nueva rama con sus nuevos cambios

--- Principal01.java
+++ Principal01.java
@@ -1,7 +1,20 @@
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Principal01 {
     public static void main(String[] args){
 
         System.out.println("Hola mundo");
+        
 
+        imprimirFechaYHoraActual();
+    }
+
+     public static void imprimirFechaYHoraActual() {
+        LocalDateTime fechaHoraActual = LocalDateTime.now();
+        DateTimeFormatter formatoFechaHora = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String fechaHoraFormateada = fechaHoraActual.format(formatoFechaHora);
+        System.out.println("Fecha y Hora actual: " + fechaHoraFormateada);
     }
 }
---
+++ README.txt
@@ -0,0 +1,8 @@
+"INFORMACION IMPORTANTE"
+
+Esto se trata de una prueba de github y estoy tratando con los repositorios de la aplicación
+
+
+"SEGUNDA ACTUALIZACION"
+
+AÑADIMOS EL METODO PARA VER LA HORA Y LA FECHA EN EL PROGRAMA PRINCIPAL.JAVA
